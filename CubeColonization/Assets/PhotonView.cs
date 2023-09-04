7fa73469c6c94f5e2fa6273d2e618b669b00e611
Hmmtrnt <rento5310@gmail.com>
2023-09-05 00:38:57 +0900

    書き直し

--- CubeColonization/Assets/PhotonView.cs
+++ CubeColonization/Assets/PhotonView.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class PhotonView : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
