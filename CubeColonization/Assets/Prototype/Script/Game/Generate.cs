be7b08fe86dee8e3c013f7a4dc0e2d7d2c3f3336
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-09-07 11:37:51 +0900

    ゲームの部分の当たり判定調整(完成ではない)

--- CubeColonization/Assets/Prototype/Script/Game/Generate.cs
+++ CubeColonization/Assets/Prototype/Script/Game/Generate.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Generate : MonoBehaviour
 {
-    // オリジナルのゲームオブジェクト
-    public GameObject _gameobject;
+    // 生成したいゲームオブジェクト
+    [SerializeField]private GameObject _gameobject;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int z = 0; z < 10; z++)
+        for (int z = -10; z < 10; z++)
         {
-            for(int x = 0; x < 10; x++)
+            for(int x = -10; x < 10; x++)
             {
                 Instantiate(_gameobject, new Vector3(x, 0.0f, z), Quaternion.identity);
             }
